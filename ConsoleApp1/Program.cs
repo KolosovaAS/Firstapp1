109fe288058c4e921e181a793281d91543e4f283
KolosovaAS <annatkachen@yandex.ru>
2024-05-17 23:15:05 +0300

    сумма элементов массива

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -70,29 +70,36 @@ namespace ConsoleApp1
 
             //Console.ReadKey();
 
+            //var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+            //int temp;
+            //for (int i =0 ; i< arr.Length; i++)
+            //{
+            //    for (int j = i+1; j < arr.Length; j++)
+            //    { 
+            //        if(arr[i]>arr[j])
+            //        {
+            //            temp = arr[i];
+            //            arr[i] = arr[j];
+            //            arr[j] = temp;
+            //        }
+            //    }
+
             var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
-            int temp;
-            for (int i =0 ; i< arr.Length; i++)
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
             {
-                for (int j = i+1; j < arr.Length; j++)
-                { 
-                    if(arr[i]>arr[j])
-                    {
-                        temp = arr[i];
-                        arr[i] = arr[j];
-                        arr[j] = temp;
-                    }
-                }
+                sum += arr[i];
                 
+            }
+            Console.Write(sum);
+                //}
+                //foreach (var item in arr)
+                //{
+                //    Console.Write(item);
+                //}
+
 
             }
-            foreach (var item in arr)
-            {
-                Console.Write(item);
-            }
-
-
-        }
     }
         enum DaysOfWeek : byte
         {
