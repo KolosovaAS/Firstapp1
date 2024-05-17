b52b6ec5cd9842c15617a67a710933b435335ab0
KolosovaAS <annatkachen@yandex.ru>
2024-05-17 13:09:23 +0300

    Перечисление

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,13 +5,34 @@ namespace ConsoleApp1
     class Program
     {
         static void Main(string[] args)
+        { 
+
+            string MyName = "Jane";
+            byte MyAge = 27;
+            bool HaveIApet = true;
+            double MyShoeSize = 37.5;
+
+            Console.WriteLine("My name is " + MyName);
+            Console.WriteLine("MyAge " + MyAge);
+            Console.WriteLine("Do I have a pet? " + HaveIApet);
+            Console.WriteLine("My shoe size is " + MyShoeSize);
+
+            //перечисление
+            DaysOfWeek MyFavoriteDay;
+
+            MyFavoriteDay = DaysOfWeek.Friday;
+
+            Console.WriteLine(MyFavoriteDay);
+
+        }
+        enum DaysOfWeek : byte
         {
-            string myName;
-            myName = "Victory";
-
-            Console.WriteLine(myName);
-
-            Console.ReadKey();
+            Tuesday,
+            Monday,
+            Wednesday,
+            Friday
         }
     }
+    
+   
 }
